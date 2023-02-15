946037fa24c40390703b672fbef419c1943fed51
R Manikandan <itismani25@gmail.com>
2023-02-15 22:30:23 +0530

    coin change naive and memoized recursion code

---
+++ coinchange.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// naive recursion
+
+int coinchange(int coins[], int n, int amount)
+{
+    if (amount == 0)
+        return 1;
+    else if (amount < 0)
+        return 0;
+
+    if (n == 0)
+        return 0;
+
+    if (amount < coins[n - 1])
+    {
+        return coinchange(coins, n - 1, amount);
+    }
+    else
+    {
+        return coinchange(coins, n, amount - coins[n - 1]) + coinchange(coins, n - 1, amount);
+    }
+}
+
+// memoization
+int coinchange(int coins[], int n, int amount, vector<vector<int>> &memo)
+{
+    if (memo[n][amount] != -1)
+        return memo[n][amount];
+
+    if (amount == 0)
+        return 1;
+    else if (amount < 0)
+        return 0;
+
+    if (n == 0)
+        return 0;
+
+    if (amount < coins[n - 1])
+    {
+        memo[n][amount] = coinchange(coins, n - 1, amount, memo);
+    }
+    else
+    {
+        memo[n][amount] = coinchange(coins, n, amount - coins[n - 1], memo) + coinchange(coins, n - 1, amount, memo);
+    }
+    return memo[n][amount];
+}
+int main()
+{
+    int coins[] = {2, 5, 3, 6};
+    int n = 4;
+    int amount = 10;
+    vector<vector<int>> memo(n + 1, vector<int>(amount + 1, -1));
+
+    cout << coinchange(coins, n, amount, memo) << endl;
+    cout << coinchange(coins, n, amount);
+}
